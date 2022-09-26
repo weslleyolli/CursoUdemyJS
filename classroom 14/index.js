82f74edfc7aa6c6a2b29299ad03e1df6bb0b59ed
Weslley <weslleyrichardson.cg@hotmail.com>
2022-09-26 13:41:14 -0300

    new classroom

---
+++ classroom 14/index.js
@@ -0,0 +1,8 @@
+let num1 = 10.21746281746; // number
+let num2 =2.5; // number
+
+console.log(num1.toString() + num2); //string temporary
+//num1 = num1.toString(); // tranforming the variable number in string
+console.log(num1.toFixed(2));
+
+
