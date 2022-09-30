c9caef2e9d24c36df97608c46e416e55a6fd8b3f
Weslley <weslleyrichardson.cg@hotmail.com>
2022-09-30 10:41:24 -0300

    commit number 6

--- classroom 15/script.js
+++ classroom 15/script.js
@@ -1,1 +1,16 @@
-const number = prompt("enter the number: ");
+let number = prompt("enter the number: ");
+number = Number(number)
+//const numberr = Number(prompt("enter the number: ")); same for sentence above
+const numberTitle = window.document.getElementById("number-title");
+const text = window.document.getElementById("text");
+
+numberTitle.innerHTML = number;
+text.innerHTML = ""
+text.innerHTML += `<p>Square root: ${number ** 0.5}. </p>`;
+text.innerHTML += `<p>${number} is integer: ${Number.isInteger(number)}. </p>`;
+text.innerHTML += `<p>is Nan: ${Number.isNaN(number)}. </p>`;
+text.innerHTML += `<p>Rounding down: ${Math.floor(number)}. </p>`;
+text.innerHTML += `<p>Rounding up: ${Math.ceil(number)}. </p>`;
+text.innerHTML += `<p>your number with 2 places: ${number.toFixed(2)}. </p>`;
+
+
---
+++ classroom 16/index.js
@@ -0,0 +1,27 @@
+const student = ["Weslley", "Camile", "Gael","Renally"];
+
+
+console.log(student.slice(0,2))// slice the list and print only the chosen
+
+
+
+//console.log(student.length);// know the length of list
+
+//delete student[1];// deleting the item in position chosen
+//student.shift(); // remove the top of list, change the index of list
+
+//student.pop() // remove th last place of list
+//const removed = student.pop()// is possible save in variable the itens removed
+
+//student.unshift("Riqulmi")// Function for add in top of the list
+//student.push("Andre")// too add in last place of list
+
+//student[student.length] = "Andre"// add to final of list
+
+
+//student[3] = "Andre";
+//student[4] = "Riquelmi"; //Add string in last place in the arraylist
+
+
+//console.log(student);
+//console.log(student[1]);
