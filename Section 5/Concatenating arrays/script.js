54b2876e869589683f2a0c6f40d8b23de1475a08
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-09 20:39:23 -0300

    concatenating arrays

---
+++ Section 5/Concatenating arrays/script.js
@@ -0,0 +1,8 @@
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+//const array3 = array1.concat(array2)
+//... rest -> ... spread
+
+const array3 = [...array1, ...array2]
+
+console.log(array3)
